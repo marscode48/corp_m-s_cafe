c6a45179c75e376501db9b0ae7c0238d2167f636
Adachi Masakazu <marscode48@gmail.com>
2023-02-22 22:07:45 +0900

    srcにjsを追加

---
+++ src/js/main.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const main = new Main();
+});
+
+class Main {
+  constructor() {
+    this.side = document.querySelector('#side-btn');
+    this.bg = document.querySelector('.bg');
+    this._init();
+  }
+
+  _init() {
+    // new MvScaleChange();
+    new MenuOpen();
+    new MenuFadeIn(520);
+    new SmoothScroll(80);
+    Pace.on('done', this._paceDone.bind(this));
+  }
+
+  _paceDone() {
+    this._scrollInit();
+  }
+
+  _inviewAnimation(el, inview) {
+    if (inview) {
+      el.classList.add('inview');
+    }
+  }
+
+  _sideAnimation(el, inview) {
+    if (inview) {
+      this.side.classList.add('inview');
+    } else {
+      this.side.classList.remove('inview');
+    }
+  }
+
+  _textAnimation(el, inview) {
+    if (inview) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  }
+
+  _fadeInAnimation(el, inview) {
+    if (inview) {
+      this.bg.classList.add('inview');
+    } else {
+      this.bg.classList.remove('inview');
+    }
+  }
+
+  _scrollInit() {
+    new ScrollObserver('.menu__item', this._inviewAnimation);
+    new ScrollObserver('.site-title', this._inviewAnimation);
+    new ScrollObserver('.cover-slide', this._inviewAnimation);
+    new ScrollObserver('.appear', this._inviewAnimation);
+    new ScrollObserver('.tween-animate-title', this._textAnimation, { rootMargin: '-100px 0px' });
+    new ScrollObserver('#menu', this._sideAnimation.bind(this), { once: false, rootMargin: '-300px 0px 0px 0px' });
+    new ScrollObserver('#location', this._fadeInAnimation.bind(this), { once: false, rootMargin: '-300px 0px 0px 0px' });
+  }
+}
